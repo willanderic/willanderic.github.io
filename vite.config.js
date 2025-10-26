1a1c7a820b8dcadc6ca8c977a033e7e4b96de589
wjwalcher <wjwalcher@gmail.com>
2025-10-25 17:59:01 -0700

    Updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/willanderic.github.io/',
   build: {
     outDir: 'docs'
   }
