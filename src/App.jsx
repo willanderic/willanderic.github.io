783a5557eebe8757eb4ea340b0f242b1858c73be
wjwalcher <wjwalcher@gmail.com>
2025-10-25 17:15:34 -0700

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import Header from './components/Header'
 import Details from './components/Details'
 import QandA from './components/QandA'
+
 import FileLinks from './components/FileLinks'
 import RSVP from './components/RSVP'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <div className="container">
-      <Header />
-      <main>
-        <Details />
-        <QandA />
-        <FileLinks />
-        {/* <RSVP /> */}
-      </main>
-    </div>
+    <>
+      <div className="container">
+        <Header />
+        <main>
+          <Details />
+          <QandA />
+          <FileLinks />
+          {/* <RSVP /> */}
+        </main>
+      </div>
+      <hr className="section-divider" />
+      <Footer />
+    </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles.css";
+
+const Footer = () => (
+  <footer className="wedding-footer">
+    <div className="footer-content">
+      <div className="footer-title">E&amp;W</div>
+      <hr className="footer-line" />
+      <div className="footer-date">10.3.2026</div>
+    </div>
+  </footer>
+);
+
+export default Footer;
