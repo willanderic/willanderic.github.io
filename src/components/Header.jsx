96198f7b5e223a8d15377218c7f6d06fbd9aa97d
wjwalcher <wjwalcher@gmail.com>
2025-11-04 22:08:35 -0800

    Addt'l font change + capitalization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="hero">
         <img src="/assets/couple.jpg" alt="Couple" className="hero-image" />
         <div className="hero-text">
-          <div className="hero-names"><span className="hero-line">Eric & William</span></div>
+          <div className="hero-names"><span className="hero-line">ERIC & WILLIAM</span></div>
           <h1><span className="hero-line">We're getting married!</span></h1>
         </div>
       </div>
